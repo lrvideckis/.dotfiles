7d507a471d86bad19abe394e2101e3eb83425aec
lrvideckis <lukevideckis@gmail.com>
2022-08-06 21:45:26 -0700

    nit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,7 +28,7 @@ vim.api.nvim_set_keymap('n', '<F5>', -- save, remove old executable, and compile
 	'<CMD>w!<CR>' ..
 	'<CMD>!rm --force %:r.out<CR>' ..
 	'<CMD>!g++ -Wall -Wextra -Wno-unused-result -Wshadow -g -std=c++20 %:r.cpp -o %:r.out<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F9>', '<CMD>!./%:r.out < in<CR>', {noremap = true}) -- run code
+vim.api.nvim_set_keymap('n', '<F9>', '<CMD>!cat in && echo "----" && ./%:r.out < in<CR>', {noremap = true}) -- run code
 vim.api.nvim_set_keymap('n', '<C-T>', '<CMD>NvimTreeFocus<CR>', {noremap = true}) -- open nvim tree
 
 -- enhancements
