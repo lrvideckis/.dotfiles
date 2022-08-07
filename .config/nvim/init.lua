8529a4612af4307ae39bbc0cb6a3ed5cd585aa58
lrvideckis <lukevideckis@gmail.com>
2022-08-06 22:20:10 -0700

    #9

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,9 +11,9 @@ vim.o.scrolloff = 2
 vim.o.cursorline = true
 vim.o.list = true -- show '>' for tabs and '-' for trailing spaces
 vim.o.termguicolors = true -- looks cooler
-vim.o.wildmode = "list:longest" -- bash-like tab completion
-vim.o.colorcolumn = "100";
-vim.o.matchpairs = "(:),{:},[:],<:>"
+vim.o.wildmode = 'list:longest' -- bash-like tab completion
+vim.o.colorcolumn = '100';
+vim.o.matchpairs = '(:),{:},[:],<:>'
 vim.g.c_no_curly_error = true -- disable curly brace error: thing[{i, j}]
 
 -- key maps
@@ -21,8 +21,8 @@ vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true}) -- the OG keymap!
 vim.api.nvim_set_keymap('i', '<TAB>', '<C-N>', {noremap = true}) -- tab completion
 vim.api.nvim_set_keymap('i', '<S-TAB>', '<C-P>', {noremap = true})
 vim.api.nvim_set_keymap('c', 'W', 'w', {noremap = true}) -- :W now writes
-vim.api.nvim_set_keymap("", '<C-J>', '6j', {noremap = true}) -- faster navigation
-vim.api.nvim_set_keymap("", '<C-K>', '6k', {noremap = true})
+vim.api.nvim_set_keymap('', '<C-J>', '6j', {noremap = true}) -- faster navigation
+vim.api.nvim_set_keymap('', '<C-K>', '6k', {noremap = true})
 vim.api.nvim_set_keymap('n', '<CR>', '<CMD>nohlsearch<CR>', {noremap = true}) -- unhighlight search results
 vim.api.nvim_set_keymap('n', '<F5>', -- save, remove old executable, and compile
 	'<CMD>w!<CR>' ..
@@ -32,8 +32,8 @@ vim.api.nvim_set_keymap('n', '<F9>', '<CMD>!cat in && echo "----" && ./%:r.out <
 vim.api.nvim_set_keymap('n', '<C-T>', '<CMD>NvimTreeFocus<CR>', {noremap = true}) -- open nvim tree
 
 -- enhancements
-vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*.cpp", command = "-r ~/programming_team_code/template.cpp" }) -- new cpp files default to template
-vim.api.nvim_create_autocmd("BufWrite", { pattern = "*.cpp,*.h,*.lua", command = "silent! execute \'%s/\\s\\+$//ge\'" }) -- remove trailing white space during writes
+vim.api.nvim_create_autocmd('BufNewFile', { pattern = '*.cpp', command = '-r ~/programming_team_code/template.cpp' }) -- new cpp files default to template
+vim.api.nvim_create_autocmd('BufWrite', { pattern = '*.cpp,*.h,*.lua', command = 'silent! execute \'%s/\\s\\+$//ge\'' }) -- remove trailing white space during writes
 require('packer').startup(function() -- :PackerSync to reload (run after all changes)
 	use 'wbthomason/packer.nvim' -- Packer can manage itself
 	use 'karb94/neoscroll.nvim' -- smooth scroll
@@ -50,7 +50,7 @@ require('packer').startup(function() -- :PackerSync to reload (run after all cha
 		tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	}
 end)
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd('colorscheme tokyonight')
 require('neoscroll').setup({ mappings = {'<C-u>', '<C-d>', 'zt', 'zz', 'zb'} }) -- importantly, not <C-E> and <C-Y>
 require('lualine').setup()
-require("nvim-tree").setup()
+require('nvim-tree').setup()
