536c9b87d8d0a1d61a37d1428e92689764f03a15
Luke Videckis <lukevideckis@gmail.com>
2022-08-07 18:34:47 -0700

    remove treesitter as it slows down nvim too much

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,23 +55,9 @@ require('packer').startup(function() -- :PackerSync to reload (run after all cha
 		},
 		tag = 'release' -- To use the latest release
 	}
-	use { -- better syntax highlighting & indenting
-		'nvim-treesitter/nvim-treesitter',
-		run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-	}
 end)
 vim.cmd('colorscheme tokyonight')
 require('neoscroll').setup({ mappings = {'<C-u>', '<C-d>', 'zt', 'zz', 'zb'} }) -- importantly, not <C-e> and <C-y>
 require('lualine').setup({ options = {theme = 'tokyonight'} })
 require('nvim-tree').setup()
 require('gitsigns').setup()
-require('nvim-treesitter.configs').setup({
-	ensure_installed = {"cpp"},
-	auto_install = true, -- Automatically install missing parsers when entering buffer
-	highlight = {
-		enable = true,
-	},
-	indent = {
-		enable = true
-	}
-})
