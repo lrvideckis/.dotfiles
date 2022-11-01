469de70daae018a8f58646cb3bea7b344cc38c86
Luke Videckis <lukevideckis@gmail.com>
2022-10-31 22:39:00 -0700

    better method of keeping terminal open

--- home/lrvideckis/.config/qtile/config.py
+++ home/lrvideckis/.config/qtile/config.py
@@ -204,7 +204,7 @@ screens = [
                 get_arrow_widget(False, False),
                 widget.Clock(
                     format="%Y-%m-%d %a %H:%M",
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' --command sh -c "cal --color=auto --year && sleep infinity"')},
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' --hold --command cal --color=auto --year')},
                     background=color2
                 ),
             ],
