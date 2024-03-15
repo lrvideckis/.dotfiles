1e194e33e31b3bcd3ba73ffa6084232848db5b43
Luke Videckis <lukevideckis@gmail.com>
2024-03-15 16:16:10 -0600

    trying new CP keybindings

--- home/lrvideckis/.config/nvim/init.lua
+++ home/lrvideckis/.config/nvim/init.lua
@@ -19,8 +19,7 @@ vim.opt.list = true -- show '>' for tabs and '-' for trailing spaces
 vim.opt.wildmode = 'list:longest' -- bash-like tab completion
 vim.opt.matchpairs = '(:),{:},[:],<:>' -- <> is not included by default; useful for c++ templates
 vim.opt.clipboard = 'unnamedplus' -- sync nvim and OS clipboard
-vim.opt.ls = 0 -- hide bottom panel
-vim.opt.ch = 0
+vim.opt.ch = 0 -- hide command prompt when unused; keep bottom panel to show command when using `:terminal ..`
 vim.opt.termguicolors = true
 vim.g.c_no_curly_error = true -- disable curly brace error: thing[{i, j}]
 
@@ -32,20 +31,37 @@ vim.api.nvim_set_keymap('c', 'W', 'w', {noremap = true}) -- :W now writes
 vim.api.nvim_set_keymap('', '<C-j>', '6j', {noremap = true}) -- faster vertical navigation
 vim.api.nvim_set_keymap('', '<C-k>', '6k', {noremap = true})
 vim.api.nvim_set_keymap('n', '<CR>', '<CMD>nohlsearch<CR>', {noremap = true}) -- unhighlight search results
-vim.api.nvim_set_keymap('n', '<F5>', -- save, remove old executable, and fast compile
-	'<CMD>w!<CR>' ..
-	'<CMD>term rm --force %:r.out && g++ -std=c++17 %:r.cpp -o %:r.out<CR>', {noremap = true})
-compile_flags = '-Wall -Wextra -Wunused -Wpedantic -Wshadow -Wlogical-op -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -Wshift-overflow=2 -Wduplicated-cond -O2 -std=c++17 -fsanitize=address,undefined -fstack-protector -D_GLIBCXX_DEBUG -D_GLIBCXX_SANITIZE_VECTOR -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_ASSERTIONS -D_FORTIFY_SOURCE=2'
-vim.api.nvim_set_keymap('n', '<F6>', -- save, remove old executable, and debug compile
-	'<CMD>w!<CR>' ..
-	'<CMD>term rm --force %:r.out && g++ ' .. compile_flags .. ' %:r.cpp -o %:r.out<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F7>', '<CMD>term cat input<CR>', {noremap = true}) -- print input file
-vim.api.nvim_set_keymap('n', '<F8>', '<CMD>term wl-paste > input && cat input<CR>', {noremap = true}) -- initialize input file with clipboard
-vim.api.nvim_set_keymap('n', '<F9>', '<CMD>term cat input && echo "----" && ./%:r.out < input<CR>', {noremap = true}) -- run code
-vim.api.nvim_set_keymap('n', '<F10>', -- save, run test
-	'<CMD>w!<CR>' ..
-	'<CMD>term oj-verify run %:r.cpp<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-t>', '<CMD>NvimTreeToggle<CR>', {noremap = true}) -- open nvim tree
+
+-- CP key maps
+exec = 'cat input && echo "----" && ./%:r.out < input'
+compile_flags = ' -Wall'
+             .. ' -Wextra'
+             .. ' -Wunused'
+             .. ' -Wpedantic'
+             .. ' -Wshadow'
+             .. ' -Wlogical-op'
+             .. ' -Wformat=2'
+             .. ' -Wfloat-equal'
+             .. ' -Wcast-qual'
+             .. ' -Wcast-align'
+             .. ' -Wshift-overflow=2'
+             .. ' -Wduplicated-cond'
+             .. ' -O2'
+             .. ' -std=c++17'
+             .. ' -fsanitize=address,undefined'
+             .. ' -fstack-protector'
+             .. ' -D_GLIBCXX_DEBUG'
+             .. ' -D_GLIBCXX_SANITIZE_VECTOR'
+             .. ' -D_GLIBCXX_DEBUG_PEDANTIC'
+             .. ' -D_GLIBCXX_ASSERTIONS'
+             .. ' -D_FORTIFY_SOURCE=2'
+vim.api.nvim_set_keymap('n', '<F5>', '<CMD>w!<CR><CMD>terminal g++ -std=c++17 %:r.cpp -o %:r.out && ' .. exec .. '<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F6>', '<CMD>w!<CR><CMD>terminal g++ ' .. compile_flags .. ' %:r.cpp -o %:r.out && ' .. exec .. '<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F7>', '<CMD>terminal cat input<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F8>', '<CMD>terminal wl-paste > input && cat input<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F9>', '<CMD>terminal ' .. exec .. '<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F10>', '<CMD>w!<CR><CMD>terminal oj-verify run %:r.cpp<CR>', {noremap = true})
 
 -- enhancements
 vim.api.nvim_create_autocmd('BufNewFile', { pattern = '*.cpp', command = 'read ~/programming_team_code/contest/template.cpp' }) -- new cpp files default to template
