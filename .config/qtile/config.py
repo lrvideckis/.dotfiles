78089fcea1a873ad0ac0999a8f2a2b4f40eb93d8
Luke Videckis <lrvideckis@gmail.com>
2025-12-31 09:27:29 -0600

    fix bug with top panel

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -140,6 +140,8 @@ screens = [
         top=bar.Bar(
             [
                 widget.GroupBox(
+                    padding_x=0,
+                    padding_y=0,
                     background=color1,
                     this_current_screen_border=color2,
                 ),
